bb3af15de9fe2496645781b3c89228d33cc0786a
Samuel Nelson <samuel.h.nelson@protonmail.com>
2023-12-08 20:45:57 -0600

    completed aoc day 6 part 1

--- aoc6.js
+++ aoc6.js
@@ -1,66 +1,66 @@
 
-let rawData = `
-Time:        54     94     65     92
-Distance:   302   1476   1029   1404
-`;
+// let rawData = `
+// Time:        54     94     65     92
+// Distance:   302   1476   1029   1404
+// `;
 
 // F O R M A T T I N G    O F    D A T A //
 
-let raceTime = [54, 94, 65, 92];
+let raceTimes = [54, 94, 65, 92];
 
-let recordDistance = [302, 1476, 1029, 1404];
+let recordDistances = [302, 1476, 1029, 1404];
 
 // F O R M A T T I N G    O F    D A T A //
 
 
 
 
-
-distanceBoatTravels >= recordDistance[i];
-
-
-
-const possibleHoldTimes = (raceTimeLimit) => {
+const possibleHoldTimes = (iteration, raceTimes, recordDistances) => {
 
     let holdTimesThatCompleteRace = [];
+    let count = 0;
 
 
-    for (let holdTime = 0; holdTime < raceTimeLimit; holdTime++) {
+    for (let holdTime = 0; holdTime < raceTimes[iteration]; holdTime++) {
 
         let speed = holdTime;
-        let raceTimeRemaining = raceTimeLimit - holdTime;
+        let raceTimeRemaining = raceTimes[iteration] - holdTime;
         let distanceBoatTravels = raceTimeRemaining * speed;
 
-        if (distanceBoatTravels >= recordDistance[holdTime]) {
+        if (distanceBoatTravels >= recordDistances[iteration]) {
 
             holdTimesThatCompleteRace.push(distanceBoatTravels);
+            count++;
 
         }
 
     }
 
+    console.log('holdtimes that complete the race:', holdTimesThatCompleteRace);
+    console.log('count of ways to win:', count);
+    return count;
+
 }
 
 
-const solve = (raceTime, recordDistance) => {
+const solve = (raceTimes, recordDistances) => {
 
-    for (let i = 0; i < raceTime.length; i++) {
+    let count = 1;
 
-        possibleHoldTimes(i);
+    for (let iteration = 0; iteration < raceTimes.length; iteration++) {
 
-
-        if (distanceBoatTravels >= recordDistance[i]) {
-
-        }
+        count *= possibleHoldTimes(iteration, raceTimes, recordDistances);
 
     }
 
+    console.log('total', count);
+
 }
 
 
 
-solve();
+solve(raceTimes, recordDistances);
 
 
 
-recordDistance / holdTime
+// recordDistances / holdTime
